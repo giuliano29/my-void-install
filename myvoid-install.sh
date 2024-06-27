a66474ebbe1e93d4896d0a6d1c111f62ebb682ee
BlackStar <66219401+giuliano29@users.noreply.github.com>
2024-06-27 11:43:30 -0300

    Update myvoid-install.sh

--- myvoid-install.sh
+++ myvoid-install.sh
@@ -3,19 +3,17 @@
 sudo xbps-install -S void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree
 sudo xbps-install -Su
 sudo xbps-install -Svy bash-completion  \
-xorg ufw xfce4 xfce4-plugins terminology xfce4-sensors-plugin gparted  \
-thunar-archive-plugin xdg-user-dirs xz unzip lightdm mousepad  \
-ristretto parole network-manager-applet NetworkManager-openvpn pulseaudio  \
-alsa-utils flatpak amdvlk amdvlk-32bit mesa-dri mesa-dri-32bit  \
-mesa-vulkan-radeon mesa-vulkan-radeon-32bit vulkan-loader vulkan-loader-32bit  \
-zlib gimp apparmor htop gnome-themes-standard elogind curl git wget nano  \
-libidn-32bit libidn2-32bit libstdc++-32bit libuuid-32bit udiskie exfatprogs  \
-gstreamer1-32bit gst-plugins-good1-32bit gst-plugins-base1-32bit xdg-user-dirs-gtk  \
-gst-plugins-bad1-32bit gst-plugins-ugly1-32bit audacity mtools dosfstools  \
-alsa-plugins-pulseaudio alsa-plugins-pulseaudio-32bit firefox xz xarchiver  \
-glu butt steam tumbler gnome-keyring geany geany-plugins gnome-calculator  \
-keepassxc flameshot obs xfwm4-themes gtk-layer-shell libimobiledevice  \
-upower libcanberra-gtk3 thunar-volman ntp rsyslogd
+xorg ufw xfce4 xfce4-plugins terminology xfce4-sensors-plugin thunar-archive-plugin  \
+xdg-user-dirs xz unzip lightdm gparted ristretto parole network-manager-applet  \
+NetworkManager-openvpn pulseaudio alsa-utils flatpak amdvlk amdvlk-32bit mesa-dri  \ 
+mesa-dri-32bit mesa-vulkan-radeon mesa-vulkan-radeon-32bit vulkan-loader vulkan-loader-32bit  \
+zlib gimp apparmor htop gnome-themes-standard curl git wget nano libidn-32bit libidn2-32bit  \
+libstdc++-32bit libuuid-32bit udiskie exfatprogs gstreamer1-32bit gst-plugins-good1-32bit  \
+gst-plugins-base1-32bit xdg-user-dirs-gtk gst-plugins-bad1-32bit gst-plugins-ugly1-32bit  \
+audacity mtools dosfstools alsa-plugins-pulseaudio alsa-plugins-pulseaudio-32bit  \
+firefox xz xarchiver glu butt steam tumbler gnome-keyring geany geany-plugins  \
+gnome-calculator keepassxc flameshot obs gtk-layer-shell libimobiledevice upower libcanberra-gtk3  \
+thunar-volman ntp rsyslogd
 
 git clone https://github.com/geany/geany-themes
 cd geany-themes && ./install.sh
